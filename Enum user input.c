502a36bd6fa9675459ceef1ded01ce553dfb0e16
ifra-byte06 <ifra.khanum021@gmail.com>
2025-11-22 10:19:04 +0530

    Enum user input.c

---
+++ Enum user input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+enum week{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+};
+int main()
+{
+ enum week today;
+ today= Friday;
+ printf("%d",today);
+}
