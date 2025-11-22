8dc11c0650060a505636ea7ca24016e229842cab
ifra-byte06 <ifra.khanum021@gmail.com>
2025-11-22 10:32:37 +0530

    Custom values enum.c

---
+++ Custom values enum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+enum Fruits{
+    Apple=5,
+    Watermelon=10,
+    Pineapple=15,
+    Strawberry=20,
+};
+int main()
+{
+enum Fruits f;
+f=Watermelon ;
+printf("Custom value of Watermelon =%d",f);
+}
